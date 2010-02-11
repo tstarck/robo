8e11e6bc1ab65ce19856a772aa2380d4728f728b
Tuomas Starck <dev@starck.fi>
2010-02-11 17:06:13 +0200

    Simo elää!

--- Simo.java
+++ Simo.java
@@ -2,15 +2,16 @@
  * Simple test of consept with a vehicle with an middle axis.
  */
 
+import lejos.nxt.LCD;
 import lejos.nxt.Button;
 import lejos.nxt.Motor;
 
-public class Drive {
+public class Simo {
 	public static Motor keula = Motor.A;
 	public static Motor akseli = Motor.B;
 	public static Motor taka = Motor.C;
 
-	public void vruum(int vauhti) {
+	public static void vruum(int vauhti) {
 		if (vauhti == 0) { keula.flt(); taka.flt(); }
 
 		keula.setSpeed(vauhti); taka.setSpeed(vauhti);
@@ -18,24 +19,29 @@ public class Drive {
 		keula.backward(); taka.backward();
 	}
 
-	public void kurvi(int kulma) {
+	public static void kurvi(int kulma) {
 		int nyt = akseli.getTachoCount();
 
-		akseli.setSpeed(30);
+		akseli.setSpeed(15);
 
-		while (nyt < kulma) {
-			System.out.println("Akseli frwd");
+		if (nyt < kulma) {
 			akseli.forward();
+			while (nyt < kulma) {
+				nyt = akseli.getTachoCount();
+				System.out.println("Fwrd > " + akseli.getTachoCount());
+			}
+			akseli.stop();
 		}
 
-		akseli.stop();
-
-		while (nyt > kulma) {
-			System.out.println("Akseli bckwrd");
+		if (nyt > kulma) {
 			akseli.backward();
+			while (nyt > kulma) {
+				nyt = akseli.getTachoCount();
+				System.out.println("Bckwrd > " + akseli.getTachoCount());
+			}
+			akseli.stop();
 		}
 
-		akseli.stop();
 		akseli.lock(99);
 	}
 
@@ -45,12 +51,15 @@ public class Drive {
 		akseli.resetTachoCount();
 		LCD.clear();
 
-		this.vruum(720);
-		try { Thread.sleep (2000); } catch (Exception e) {}
-		this.vruum(360);
+		vruum(720);
+		try { Thread.sleep (3000); } catch (Exception e) {}
+		vruum(360);
 		kurvi(15);
 		try { Thread.sleep (2000); } catch (Exception e) {}
+		kurvi(-30);
+		try { Thread.sleep (4000); } catch (Exception e) {}
 		kurvi(0);
+		vruum(0);
 
 		System.out.println("End.");
 		Button.ESCAPE.waitForPressAndRelease();
