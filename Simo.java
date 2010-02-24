90529120d9d013db02d937815c150270a08f5b26
Tuomas Starck <dev@starck.fi>
2010-02-24 13:37:01 +0200

    Cleanup.

--- Simo.java
+++ Simo.java
@@ -7,46 +7,6 @@ import lejos.nxt.Button;
 import lejos.nxt.Motor;
 
 public class Simo {
-	/*
-	public static Motor keula = Motor.A;
-	public static Motor akseli = Motor.B;
-	public static Motor taka = Motor.C;
-
-	public static void vruum(int vauhti) {
-		if (vauhti == 0) { keula.flt(); taka.flt(); }
-
-		keula.setSpeed(vauhti); taka.setSpeed(vauhti);
-
-		keula.backward(); taka.backward();
-	}
-
-	public static void kurvi(int kulma) {
-		int nyt = akseli.getTachoCount();
-
-		akseli.setSpeed(15);
-
-		if (nyt < kulma) {
-			akseli.forward();
-			while (nyt < kulma) {
-				nyt = akseli.getTachoCount();
-				System.out.println("Fwrd > " + akseli.getTachoCount());
-			}
-			akseli.stop();
-		}
-
-		if (nyt > kulma) {
-			akseli.backward();
-			while (nyt > kulma) {
-				nyt = akseli.getTachoCount();
-				System.out.println("Bckwrd > " + akseli.getTachoCount());
-			}
-			akseli.stop();
-		}
-
-		akseli.lock(99);
-	}
-	*/
-
 	public static void main(String[] limbo) {
 		WiiNXT ohjain = WiiNXT.getInstance();
 
@@ -54,29 +14,9 @@ public class Simo {
 
 		while (!Button.ENTER.isPressed()) {
 			ohjain.read();
-			System.out.println("Pitch::" + ohjain.pitch());
+			System.out.println("pitch : " + ohjain.pitch());
 			if (Button.ESCAPE.isPressed()) ohjain.setRumble(true);
 			else ohjain.setRumble(false);
 		}
-
-		/*
-		System.out.println("Vruum?");
-		Button.ENTER.waitForPressAndRelease();
-		akseli.resetTachoCount();
-		LCD.clear();
-
-		vruum(720);
-		try { Thread.sleep (3000); } catch (Exception e) {}
-		vruum(360);
-		kurvi(15);
-		try { Thread.sleep (2000); } catch (Exception e) {}
-		kurvi(-30);
-		try { Thread.sleep (4000); } catch (Exception e) {}
-		kurvi(0);
-		vruum(0);
-
-		System.out.println("End.");
-		Button.ESCAPE.waitForPressAndRelease();
-		*/
 	}
 }
